da027703549907bedacfc39176577908edaf0fc2
quan-nguyen-thuc06 <quannguyenthuc06@gmail.com>
2024-04-06 21:30:30 +0700

    add Handle Inhale smoke

--- Assets/Scripts/Player/PlayerHoldBreatheState.cs
+++ Assets/Scripts/Player/PlayerHoldBreatheState.cs
@@ -6,7 +6,7 @@ public class PlayerHoldBreatheState : PlayerState
 {
     public PlayerHoldBreatheState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
     {
-        
+
     }
 
     public override void Enter()
@@ -18,16 +18,37 @@ public class PlayerHoldBreatheState : PlayerState
     {
         base.Exit();
     }
-    
+
     public override void Update()
     {
         base.Update();
-        
+
         player.IncreaseCarbonDioxide();
         player.DecreaseOxygen();
-        if(Input.GetKey(KeyCode.I))
+        if (Input.GetKey(KeyCode.I))
             player.stateMachine.ChangeState(player.inhaleState);
         else if (Input.GetKey(KeyCode.O))
             player.stateMachine.ChangeState(player.exhaleState);
+    }
+
+    void HandleInhaleSmoke()
+    {
+        double probability = 0.7;
+
+        if (ShouldCallFunction(probability))
+        {
+            player.stateMachine.ChangeState(player.coughState);
+        }
+        else
+        {
+            player.stateMachine.ChangeState(player.inhaleState);
+        }
+    }
+
+    bool ShouldCallFunction(double probability)
+    {
+        double randomValue = Random.value;
+
+        return randomValue < probability;
     }
 }
