0af9dc8815bce1e74d7e902722f5c31eb5d4a4fe
quan-nguyen-thuc06 <quannguyenthuc06@gmail.com>
2024-04-06 17:55:32 +0700

    update test Smoke Scene

--- Assets/Scripts/Smoke.cs
+++ Assets/Scripts/Smoke.cs
@@ -7,11 +7,39 @@ public class Smoke : MonoBehaviour
     [SerializeField]
     private ParticleSystem smokeParticle;
 
+    private float rateOverTime = 15;
+
     private bool isPlaying = false;
     // Start is called before the first frame update
     void Start()
     {
+        var emission = smokeParticle.emission;
+        emission.rateOverTime = rateOverTime;
+    }
 
+    public void ReduceSmoke()
+    {
+        StartCoroutine(ReduceSmokeOverTime());
+    }
+
+    private IEnumerator ReduceSmokeOverTime(float duration = 1f)
+    {
+        float elapsedTime = 0f;
+        float startRate = smokeParticle.emission.rateOverTime.constant;
+        float endRate = 0f;
+
+        var emission = smokeParticle.emission;
+        while (elapsedTime < duration)
+        {
+            float rate = Mathf.Lerp(startRate, endRate, elapsedTime / duration);
+            emission.rateOverTime = rate;
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        emission.rateOverTime = endRate;
+
+        HideSmoke();
     }
 
     public void ShowSmoke()
