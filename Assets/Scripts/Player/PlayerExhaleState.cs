3f8105290b4a4a385b86026738232e83eaf2c1bd
quan-nguyen-thuc06 <quannguyenthuc06@gmail.com>
2024-04-21 01:36:54 +0700

    fix in/exhale

--- Assets/Scripts/Player/PlayerExhaleState.cs
+++ Assets/Scripts/Player/PlayerExhaleState.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class PlayerExhaleState : PlayerState
 {
+    Tween counterTween;
+
     public PlayerExhaleState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
     {
     }
@@ -11,14 +14,15 @@ public class PlayerExhaleState : PlayerState
     public override void Enter()
     {
         base.Enter();
-        player.StartCoroutine(ChangeHoldBreatheStateStateAfterDelay());
+        ChangeHoldBreatheStateStateAfterDelay();
     }
 
-    private IEnumerator ChangeHoldBreatheStateStateAfterDelay()
+    private void ChangeHoldBreatheStateStateAfterDelay()
     {
+        counterTween?.Kill();
         var time = GameManager.instance.gameConfig.maxTimeExhale;
-        yield return new WaitForSeconds(time);
-        stateMachine.ChangeState(player.holdBreatheState);
+        counterTween = DOVirtual.Float(0, 1, time, (v) => { })
+        .OnComplete(() => player.stateMachine.ChangeState(player.holdBreatheState));
     }
 
     public override void Exit()
--- Assets/Scripts/Player/PlayerInhaleState.cs
+++ Assets/Scripts/Player/PlayerInhaleState.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class PlayerInhaleState : PlayerState
 {
+    Tween counterTween;
     public PlayerInhaleState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
     {
     }
@@ -11,14 +13,15 @@ public class PlayerInhaleState : PlayerState
     public override void Enter()
     {
         base.Enter();
-        player.StartCoroutine(ChangeHoldBreatheStateStateAfterDelay());
+        ChangeHoldBreatheStateStateAfterDelay();
     }
 
-    private IEnumerator ChangeHoldBreatheStateStateAfterDelay()
+    private void ChangeHoldBreatheStateStateAfterDelay()
     {
+        counterTween?.Kill();
         var time = GameManager.instance.gameConfig.maxTimeInhale;
-        yield return new WaitForSeconds(time);
-        stateMachine.ChangeState(player.holdBreatheState);
+        counterTween = DOVirtual.Float(0, 1, time, (v) => { })
+        .OnComplete(() => player.stateMachine.ChangeState(player.holdBreatheState));
     }
 
     public override void Exit()
