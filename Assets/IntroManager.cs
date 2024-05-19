961ca66cd285992272922b55d1e3b9cddbb494ae
quan-nguyen-thuc06 <quannguyenthuc06@gmail.com>
2024-05-19 14:38:07 +0700

    add intro water level

--- Assets/IntroManager.cs
+++ Assets/IntroManager.cs
@@ -10,6 +10,9 @@ public class IntroManager : MonoBehaviour
 
     public GameObject cat;
 
+    public PlayableAsset introNormal;
+    public PlayableAsset introLevel3;
+
     public void Init()
     {
         intro.stopped += (director) =>
@@ -27,6 +30,15 @@ public class IntroManager : MonoBehaviour
 
     public void PlayIntro()
     {
+        if (GameManager.instance.currentLevel == 3)
+        {
+            intro.playableAsset = introLevel3;
+        }
+        else
+        {
+            intro.playableAsset = introNormal;
+        }
+
         intro.Stop();
         intro.Play();
     }
