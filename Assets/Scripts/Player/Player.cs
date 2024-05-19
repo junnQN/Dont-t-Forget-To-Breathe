282fa9d69c7b7e52151ae99572a9444b4e888700
quan-nguyen-thuc06 <quannguyenthuc06@gmail.com>
2024-05-19 14:45:31 +0700

    fix sprint water

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -371,7 +371,7 @@ public class Player : MonoBehaviour
         }
         else if (other.CompareTag("FlushButton"))
         {
-            isTouchFlushButton = true;
+            isTouchFlushButton = false;
         }
     }
 
