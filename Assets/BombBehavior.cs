83d4ea8b28337524f8e35ce2cb8fcf93e4462316
quan-nguyen-thuc06 <quannguyenthuc06@gmail.com>
2024-05-24 22:28:41 +0700

    fix bomb

--- Assets/BombBehavior.cs
+++ Assets/BombBehavior.cs
@@ -39,7 +39,7 @@ public class BombBehavior : MonoBehaviour
 
     void Update()
     {
-        if (isPlaying == false)
+        if (isPlaying == false || GameManager.instance.isPlaying == false)
         {
             return;
         }
